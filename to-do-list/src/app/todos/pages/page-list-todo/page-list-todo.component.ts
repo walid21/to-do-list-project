c9c0b5798564a6f2a4699692f76ba8fae09baadd
Walid Jamili <wal.jam21@gmail.com>
2023-09-11 16:16:46 +0200

    page list mise en page et connexion au json

--- to-do-list/src/app/todos/pages/page-list-todo/page-list-todo.component.ts
+++ to-do-list/src/app/todos/pages/page-list-todo/page-list-todo.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/core/models/todo';
+import { TodosService } from '../../services/todos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-list-todo',
   templateUrl: './page-list-todo.component.html',
-  styleUrls: ['./page-list-todo.component.scss']
+  styleUrls: ['./page-list-todo.component.scss'],
 })
 export class PageListTodoComponent {
+  public tab!: Todo[];
 
+  constructor(private todosService: TodosService, private router: Router) {
+    this.todosService.getData().subscribe((data) => {
+      this.tab = data;
+      console.log(this.tab);
+    });
+  }
 }
