4ce7aff3d78192465dd88a90d959e86e71cb5e56
Julie Thomas <ladyjulie63@gmail.com>
2023-09-11 14:38:18 +0200

    changement todo->todos

--- to-do-list/src/app/app-routing.module.ts
+++ to-do-list/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/todo', pathMatch: 'full' },
+  { path: '', redirectTo: '/todos', pathMatch: 'full' },
   {
     path: 'todos',
     loadChildren: () =>
