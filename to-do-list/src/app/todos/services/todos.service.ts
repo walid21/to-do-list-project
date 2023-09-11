898f77121d9aa08059e0ae548a137639835e220e
Julie Thomas <ladyjulie63@gmail.com>
2023-09-11 14:55:52 +0200

    Folder service

---
+++ to-do-list/src/app/todos/services/todos.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from 'src/app/core/models/todo';
+import { StateTodo } from 'src/app/core/enums/state-todo';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TodosService {
+  constructor(private http: HttpClient) {}
+  configUrl = ' http://localhost:3000/todos';
+
+  public changeState(todo: Todo, stateTodo: StateTodo): Observable<Todo> {
+    const todoNew = new Todo(todo);
+    todoNew.stateTodo = stateTodo;
+    return this.update(todoNew);
+  }
+
+  public update(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.configUrl}/${todo.id}`, todo);
+  }
+}
