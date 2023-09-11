d881de5f1874a415a4b2b4a64190ec3a50d92472
Walid Jamili <wal.jam21@gmail.com>
2023-09-12 01:45:14 +0200

    amelioration de la mise en page et ajout de l'input et de la fonctionnalité pour ajouté une tache

--- to-do-list/src/app/core/models/todo.ts
+++ to-do-list/src/app/core/models/todo.ts
@@ -1,13 +1,17 @@
-import { StateTodo } from "../enums/state-todo";
-import { TodoI } from "../interfaces/todo-i";
+import { StateTodo } from '../enums/state-todo';
+import { TodoI } from '../interfaces/todo-i';
 
-export class Todo implements TodoI{
-    stateTodo = StateTodo.EN_COURS;
-    id!: number;
-    title = "default";
-    constructor(obj?: Partial<Todo>){
-        if(obj){
-            Object.assign(this, obj);
-        }
+export class Todo implements TodoI {
+  stateTodo = StateTodo.EN_COURS;
+  id!: number;
+  title!: string;
+  index: number = 5;
+  constructor(obj?: Partial<Todo>) {
+    if (obj) {
+      Object.assign(this, obj);
+    } else if (!this.id) {
+      this.index++;
+      this.id = this.index;
     }
+  }
 }
