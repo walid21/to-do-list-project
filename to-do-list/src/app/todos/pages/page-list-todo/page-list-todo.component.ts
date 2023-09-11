3bed5b6c7d76098716ae4e37b3308307b0302cd2
Walid Jamili <wal.jam21@gmail.com>
2023-09-11 15:08:44 +0200

    ajout de la page list todo

---
+++ to-do-list/src/app/todos/pages/page-list-todo/page-list-todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list-todo',
+  templateUrl: './page-list-todo.component.html',
+  styleUrls: ['./page-list-todo.component.scss']
+})
+export class PageListTodoComponent {
+
+}
