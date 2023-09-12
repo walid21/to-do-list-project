111ae9f6e14cf408abc915ec13b200c5db1ab636
Walid Jamili <wal.jam21@gmail.com>
2023-09-12 03:57:41 +0200

    ajout de la partie du code pour gerer le changement d'etat en fonction de la checkbox

--- to-do-list/src/app/core/models/todo.ts
+++ to-do-list/src/app/core/models/todo.ts
@@ -5,13 +5,10 @@ export class Todo implements TodoI {
   stateTodo = StateTodo.EN_COURS;
   id!: number;
   title!: string;
-  index: number = 5;
+
   constructor(obj?: Partial<Todo>) {
     if (obj) {
       Object.assign(this, obj);
-    } else if (!this.id) {
-      this.index++;
-      this.id = this.index;
     }
   }
 }
