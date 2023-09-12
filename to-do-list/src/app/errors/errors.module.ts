8f54dbf8da767033f1116e92464752c89c8b18ee
frenchdogue <thomas.puaud@orange.fr>
2023-09-12 15:59:47 +0200

    composant bandeau superieur et ajout à la page 404

--- to-do-list/src/app/errors/errors.module.ts
+++ to-do-list/src/app/errors/errors.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ErrorsRoutingModule } from './errors-routing.module';
+import { TodosModule } from '../todos/todos.module';
 
 
 
@@ -12,6 +13,7 @@ import { ErrorsRoutingModule } from './errors-routing.module';
   imports: [
     CommonModule,
     ErrorsRoutingModule,
+    TodosModule
   ]
 })
 export class ErrorsModule { }
